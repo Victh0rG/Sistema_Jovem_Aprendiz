be0fd4e9f08588b1eff4c074d883d3fcf510a5f3
Victhor Gabriel <105661120+Victh0rG@users.noreply.github.com>
2025-12-19 10:15:49 -0300

    json

--- back-end/src/modules/routes/index.js
+++ back-end/src/modules/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
 
+
+app.use(express.json());
 /* GET home page. */
 router.get('/', (req, res, next) => {
   //res.render('index', { title: 'Express' });
   res.send('respond with a resource');
+
+  res.json({ mesage: 'Json Funcionanndo'})
   next();
 });
 
