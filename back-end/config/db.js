9d243b25acccc6e1f46c253744abda4e0b7a3868
Victhor Gabriel <105661120+Victh0rG@users.noreply.github.com>
2025-12-19 10:13:08 -0300

    create conection with db

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,1 +1,16 @@
 //conexão msql
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+// Create connection
+
+const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Ajuste conforme necessário
+    queueLimit: 0
+});
+
+module.exports = connection;
